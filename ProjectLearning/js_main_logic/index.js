0053af629f5493ebbcae0d16c89bbad7cee3ff24
brvinxdvnce <ivanovdanil.hits2024@gmail.com>
2025-03-24 16:28:48 +0300

    idk just turning react

--- ProjectLearning/js_main_logic/index.js
+++ ProjectLearning/js_main_logic/index.js
@@ -1,1 +1,13 @@
-console.log("test log")
+ReactDOM.render(<h1>HI</h1>, document.getElementById("app"))
+console.log("Program Is working!")
+
+
+
+// const inputClick= () => console.log("Clicked")
+// const mouseOver = () => console.log("Mouse Over")
+// const elements = <input placeholder="Help text"
+//     onClick={inputClick} onMouseEnter={mouseover} />
+
+// const app = document.getElementById("app")
+
+// ReactDOM.render(elements, app)
