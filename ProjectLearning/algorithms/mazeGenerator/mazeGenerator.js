02f33718f75e1c6cee64c0d1310b23aa423b6618
brvinxdvnce <ivanovdanil.hits2024@gmail.com>
2025-04-04 17:41:33 +0300

    АСТАР РАБОТАЕТ!!!!!

--- ProjectLearning/algorithms/mazeGenerator/mazeGenerator.js
+++ ProjectLearning/algorithms/mazeGenerator/mazeGenerator.js
@@ -1,3 +1,9 @@
+function getMinQuEl(q) {
+    if (!q) return;
+    let minElem = q[0];
+    for (elem of q) minElem = elem.f < minElem.f ? elem : minElem;
+    return minElem;
+}
 
 function getRandomInt(min, max) {
     if (min == max) return min;
@@ -42,8 +48,21 @@ function getAdjacencyMatrixFromMaze(maze) {
     return adjacencyMatrix;
 }
 
+class Cell {
+    constructor(x, y, parent = null) {
+      this.x = x;
+      this.y = y;
+      this.parent = parent;
+      this.g = 0; // Манхэттенское расстояние от старта
+      this.h = 0; // Манхэттенское расстояние до финиша 
+      this.f = 0; // Общая стоимость (g + h)
+    }
+  }
+
 class Grid {
-    constructor(cellCountInSide) {
+    constructor(cellCountInSide = 21) {
+        document.getElementById('size_input').value
+        
         this.canvas = document.getElementById('maze_field');
         this.ctx = this.canvas.getContext('2d');
 
@@ -82,6 +101,9 @@ class Grid {
                     case 5: //пограничные вершины для алгоритма А*
                         this.ctx.fillStyle = "rgb(237, 138, 0)";
                         break;
+                    case 6: //путь А*
+                        this.ctx.fillStyle = "rgb(85, 124, 216)";
+                        break;
                 }
 
                 this.ctx.fillRect(
@@ -90,6 +112,14 @@ class Grid {
                     (j + 1) * this.cellHeight,
                     (i + 1) * this.cellWidth,);
             }
+        }
+    }
+
+    drawWay(way) {
+        let cell;
+        for (cell of way) {
+            this.grid[cell.y][cell.x] = 6;
+            this.draw();
         }
     }
 
@@ -198,4 +228,123 @@ class Grid {
         }
         this.draw();
     }
-}
+
+    //манхэттенское расстояние, по сути, длина треугольника, где start и end - это вершины
+    // на концах гипотенузы прямоугольного треугольника, а расстояние - длина катетов
+    distance(node1, node2) {
+        return Math.abs(node1.x - node2.x) + Math.abs(node1.y - node2.y);
+    }
+
+    goAStar(startPoint, endPoint) {
+        let startCell = new Cell(startPoint.x, startPoint.y);
+        let endCell = new Cell(endPoint.x, endPoint.y);
+        
+        // очередь - все пограничные вершины. на поле она немного напоминает очередь в обходе в ширину
+        let queue = [];
+        queue.push(startCell)
+
+        const visited = new Set();
+        // направления движения (вверх, вниз, влево, вправо)
+        const directions = [
+            { dx:  0, dy: -1},
+            { dx:  0, dy:  1},
+            { dx: -1, dy:  0},
+            { dx:  1, dy:  0},
+        ];
+
+        // как это работает? из граничной очереди берется ближайшая к финишу точка,
+        // проверяется на финиш : если да, то строится путь start - end, иначе берутся
+        // соседи этой точки, добавляются в очередь и процесс начинается заново
+        // если очередь опустела (буквально, проверили всю компоненту),
+        // но финиша не было - лютый анлак, значит пути нет
+
+        while (queue.length > 0 ) {
+            // Найти узел с минимальной стоимостью в очереди -> изьять его -> отметить в visited (как строку!)
+            let currentNode = getMinQuEl(queue);
+            queue.splice(queue.indexOf(currentNode), 1);
+            visited.add(`${currentNode.x},${currentNode.y}`);
+            this.grid[currentNode.y][currentNode.x] = 4;
+
+            // Проверка достижения цели
+            if (currentNode.x === endPoint.x && currentNode.y === endPoint.y) {
+                const way = [];
+                let current = currentNode;
+                while (current !== null) {
+                    way.push({ x: current.x, y: current.y });
+                    current = current.parent;
+                }
+                this.drawWay(way);
+                return;
+            }
+
+            for (const dir of directions) {
+                const x = currentNode.x + dir.dx;
+                const y = currentNode.y + dir.dy;
+
+                if (y < 0 || y >= this.cellCountInSide ||
+                    x < 0 || x >= this.cellCountInSide) continue;
+                if (this.grid[y][x] === 1) continue;
+                if (visited.has(`${x},${y}`)) continue;
+                const neighbor = new Cell(x, y, currentNode);
+
+                // Вычислить стоимости
+                neighbor.g = currentNode.g + 1;
+                neighbor.h = this.distance(neighbor, endPoint);
+                neighbor.f = neighbor.g + neighbor.h;
+
+                // Проверить, есть ли сосед в queue с лучшим g
+                const existingNode = queue.find(n => n.x === x && n.y === y);
+                if (existingNode) {
+                    if (neighbor.g < existingNode.g) {
+                        existingNode.g = neighbor.g;
+                        existingNode.f = neighbor.f;
+                        existingNode.parent = currentNode;
+                    }
+                } else {
+                    queue.push(neighbor);
+                    this.grid[neighbor.y][neighbor.x] = 5;
+                }
+            }
+        }
+        return; // Путь не найден
+        }
+    }
+
+
+    // let startPoint_x = startPoint.x;
+    // let startPoint_y = startPoint.y;
+    // let endPoint_x = endPoint.x;
+    // let endPoint_y = endPoint.y;
+    
+    // let adjacencyMatrix = getAdjacencyMatrixFromMaze(this.grid);
+    // let queue = []; //текущая очередь на рассмотрение
+    // let closedSet = []; //то, где мы уже были
+    // let finalWay = [];//путь от старта до финиша
+
+    // let start = new MazeCell(startPoint_y, startPoint_x);
+    // let end = new MazeCell(endPoint_y, endPoint_x);
+
+    // queue.push(start);
+    // while (queue || queue.length) {
+    //     let p = queue[0];
+    //     queue.shift();
+
+    //     if (x in clodedSet) continue;
+
+    //     if (x == end) return p;
+
+    //     for (vertex of getAdjacentVertices(this.grid, i, j)){
+    //         queue.push(vertex);
+    //     }
+    // }
+    // return -1;
+    // }
+
+
+
+    // function sss() {
+    //     let size = parseInt(document.getElementById('size_input').value) || 21;
+    //     canv = new Grid(size || 21);
+    //     canv.draw();
+    //     canv.generateMaze();
+    //}
